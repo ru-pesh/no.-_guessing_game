9539b7eeeee5889e8a20c0c7159e672ccc2efd3d
ru-pesh <rupeshbhandari3750@gmail.com>
2023-05-23 18:20:44 +0545

    Initial commit

---
+++ guess.py
@@ -0,0 +1,19 @@
+import random
+print('\n\n\n\t\t\tNUMBER GUESSING GAME')
+print('\nRULES:\n1.You get five chances to guess.\n2.you will get hint after your first chance.')
+print('3.The no. guessed by you and computer should be same inorder to win the game.')
+b=random.randint(1,10)
+cou=1
+while cou<6 :
+    a = int(input('\nGuess a no. between 1 and 100 : '))
+    if a==b:
+        print('You guessed it correctly, it\'s:',b,'\nYOU WIN!')
+        break
+    elif a>b and cou !=5:
+        print('\nThe correct no. is less than',a,'\nyou still have',5-cou,'chance left')
+    elif a<b and cou !=5:
+        print('\nThe correct no. is greater than',a,'\nyou still have',5-cou,'chance left')
+    else:
+        print('\n\nYou are incorrect and Your chances are over \nThe correct no. is',b,'\nYOU LOSE!')
+    cou+=1
+
